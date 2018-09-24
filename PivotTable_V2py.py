76db274f64edd20f9bb86d44acfe59370ab7a8ae
mws75 <mwspencer75@gmail.com>
2018-09-24 15:39:46 -0700

    Unpivot File Created

---
+++ PivotTable_V2py.py
@@ -0,0 +1,59 @@
+#These are funcitons you can use to manipulate a pivot table 
+#V2 - Cleaned Up Code 
+
+import pandas as pd 
+import numpy
+
+def Unpivot(df, numOfRowHeaders = 1):
+    #use to unpivot a table
+
+    #Get data frame counts
+    numOfRows = len(df.index)
+    numOfColumns = len(df.columns)  #We don't need to subtract anything because the range(1, numOfColumns) takes care of that    
+
+    #print(df)
+    #create columns
+    listOfColumnNames = list()
+    for rowHeader in range(numOfRowHeaders):
+        columnName = str(df.columns[rowHeader])
+        listOfColumnNames.append(columnName)
+    listOfColumnNames.append("ColumnTitle")
+    listOfColumnNames.append("Value")
+
+    unpivotedDf = pd.DataFrame(columns= listOfColumnNames, dtype=str)
+
+    rowCount = 0   
+    for row in range(numOfRows):        
+        for col in range(numOfRowHeaders, numOfColumns):
+            #create a list that will be added as a row to the data table. 
+            unpivotedRowConstructor = list()        
+            #add static to unpivotted table
+            for i in range(numOfRowHeaders):
+                unpivotedRowConstructor.append(str(df.iloc[row, i]))
+                        
+            #add dynamic data to unpivottedtable
+            unpivotedRowConstructor.append(str(df.columns[col]))
+            unpivotedRowConstructor.append(str(df.iloc[row,col]))            
+            #add row to datatable
+            unpivotedDf.loc[rowCount] = unpivotedRowConstructor
+            rowCount += 1
+
+    return unpivotedDf     
+
+
+def main():
+    sample = r"H:\20180719_LUX_to_Lipper_Formating\20180906_PythonAutomation\BikeShop.csv"
+    liveDataSample = r"H:\20180719_LUX_to_Lipper_Formating\20180906_PythonAutomation\Lipper_Raw__ESL - Table (1).csv"
+    df = pd.DataFrame.from_csv(liveDataSample)
+    numberOfRows = pd.DataFrame(df)
+    try:
+        numberOfRows = Unpivot(df, 9)
+    except:
+        print("Unable to unpivot")
+        return
+    
+    print(numberOfRows)
+        
+    pd.DataFrame.to_csv(numberOfRows, "endSample.csv")
+if __name__ == "__main__":
+    main()
